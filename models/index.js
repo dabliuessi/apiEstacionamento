fbec50fb7b43a6192090d160e4d3487c4ccfbc5d
dabliuessi <wsmdm555@gmail.com>
2025-06-19 14:57:12 -0300

    commit02

--- models/index.js
+++ models/index.js
@@ -3,14 +3,16 @@ import Veiculo from './Veiculo.js';
 import Estacionamento from './Estacionamento.js';
 import RegistroAcesso from './RegistroAcesso.js';
 
-
+// Usuário <-> Veículo
 Veiculo.belongsTo(Usuario, { foreignKey: 'id_usuario' });
 Usuario.hasMany(Veiculo, { foreignKey: 'id_usuario' });
 
-RegistroAcesso.belongsTo(Veiculo, { foreignKey: 'id_veiculo' });
-RegistroAcesso.belongsTo(Estacionamento, { foreignKey: 'id_estacionamento' });
-
+// Registro <-> Veículo
+RegistroAcesso.belongsTo(Veiculo, { foreignKey: 'id_veiculo', as: 'veiculo' });
 Veiculo.hasMany(RegistroAcesso, { foreignKey: 'id_veiculo' });
+
+// Registro <-> Estacionamento
+RegistroAcesso.belongsTo(Estacionamento, { foreignKey: 'id_estacionamento', as: 'estacionamento' });
 Estacionamento.hasMany(RegistroAcesso, { foreignKey: 'id_estacionamento' });
 
 export { Usuario, Veiculo, Estacionamento, RegistroAcesso };
